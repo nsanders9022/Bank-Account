005a7d751ca20cf619b46ad49af578f0bc54865f
Nicole Sanders and sandro mijatovic <nicole@>
2017-01-24 16:43:18 -0800

    dysfunctional bank account

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,48 @@
-// var deposit = 0;
-// var withdrawl = 0;
-// var initialDeposit = 0;
+var masterAccount
 
 function Balances(initialDeposit, name1) {
   this.initialDeposit = initialDeposit;
-  this.name1 = name;
-  this.deposit = deposit;
-  this.withdrawal = withdrawal;
+  this.name1 = name1;
 }
 
-Balances.prototype.manipulate = function(initialDeposit, deposit, withdrawal) {
-  if (deposit > 0) {
+Balances.prototype.addMoney = function() {
     this.initialDeposit += deposit;
-  }
-  if (withdrawal > 0) {
-    this.initialDeposit = this.initialDeposit - withdrawal;
-  }
+}
 
-  if (deposit === NaN) {
-    this.deposit = 0;
-  }
-
-  if (withdrawal === NaN) {
-    this.withdrawal = 0;
-  }
-
-  return this.initialDeposit;
+Balances.prototype.subtractMoney = function() {
+    this.initialDeposit -= withdrawal;
 }
 
 $(function() {
   $("#bank").submit(function(event) {
     event.preventDefault();
 
-
-
-    var initialDeposit = parseInt($("#initialDeposit").val());
     var name = $("#name").val();
+    var initialDeposit = parseInt($("#initialDeposit").val());
 
+    masterAccount = new Balances(initialDeposit, name)
 
-
-    var obj = new Balances(initialDeposit, name);
-
+    $("#transactions").text(masterAccount.initialDeposit)
 
     $("#initialDeposit").val("");
-    $("#deposit").val("");
-    $("#withdrawal").val("");
+    $("#name").val("");
 
-    $("#transactions").prepend("<li>" + obj.manipulate() + "</li>");
   })
 
   $("#math").submit(function(event) {
-    event.preventDefault();
-
-    var initialDeposit = parseInt($("#initialDeposit").val());
     var deposit = parseInt($("#deposit").val());
     var withdrawal = parseInt($("#withdrawal").val());
 
-    var obj = new Balances(initialDeposit, name);
+    if (deposit > 0) {
+      masterAccount.addMoney();
+    }
 
-    obj.manipulate(initialDeposit, deposit,withdrawal)
+    if (withdrawal > 0) {
+      masterAccount.subtractMoney();
+    }
 
-
+    $("#transactions").text(masterAccount.initialDeposit);
   })
+
+
 })
